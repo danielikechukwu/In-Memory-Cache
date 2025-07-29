269d61713d4178379ff815dfb681c0f692bab58a
Daniel ikechukwu <ikechukwudaniel080@gmail.com>
2025-07-29 14:02:36 +0100

    feat(registering): Implement the registration of repository and service.
    Add migration, and implement connection string.

--- InMemoryCachingExecution/Program.cs
+++ InMemoryCachingExecution/Program.cs
@@ -1,3 +1,7 @@
+using InMemoryCachingExecution.Data;
+using InMemoryCachingExecution.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register the InMemoryCachingDbContext with SQL Server connection string
+builder.Services.AddDbContext<InMemoryCachingDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register in-memory caching service for caching data in RAM
+builder.Services.AddMemoryCache();
+
+//Register LocationRepository as a Scoped Service
+builder.Services.AddScoped<LocationRepository>();
 
 var app = builder.Build();
 
